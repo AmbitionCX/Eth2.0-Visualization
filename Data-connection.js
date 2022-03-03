e7ac60479f4f83029a4707f224158e25c041d2d9
AmbitionCX <chenxuanamazing@gmail.com>
2022-03-04 00:31:28 +0800

    Changes to be committed: 	new file:   Data-connection.js 	renamed:    Available-Parameters.md -> Guidance/Available-Parameters.md 	renamed:    metrics_endpoints.json -> Guidance/metrics_endpoints.json 	modified:   README.md 	new file:   Vue-template/READ.md 	renamed:    client/index.html -> Vue-template/client/index.html 	renamed:    client/package-lock.json -> Vue-template/client/package-lock.json 	renamed:    client/package.json -> Vue-template/client/package.json 	renamed:    client/public/favicon.ico -> Vue-template/client/public/favicon.ico 	renamed:    client/src/App.vue -> Vue-template/client/src/App.vue 	renamed:    client/src/assets/document.json -> Vue-template/client/src/assets/document.json 	renamed:    client/src/components/ControlPanel.vue -> Vue-template/client/src/components/ControlPanel.vue 	renamed:    client/src/components/DataTable.vue -> Vue-template/client/src/components/DataTable.vue 	renamed:    client/src/main.js -> Vue-template/client/src/main.js 	renamed:    client/src/store/index.js -> Vue-template/client/src/store/index.js 	renamed:    client/vite.config.js -> Vue-template/client/vite.config.js 	renamed:    server/requirements.txt -> Vue-template/server/requirements.txt 	renamed:    server/run.py -> Vue-template/server/run.py 	renamed:    server/src/__init__.py -> Vue-template/server/src/__init__.py 	renamed:    server/src/models.py -> Vue-template/server/src/models.py 	renamed:    server/src/views.py -> Vue-template/server/src/views.py
     Untracked files:
    	.env

---
+++ Data-connection.js
@@ -0,0 +1,60 @@
+const createConnectionPool = require('@databases/pg');
+const {sql} = require('@databases/pg');
+const PrometheusQuery = require('prometheus-query');
+const dotenv = require('dotenv').config();
+const axios = require('axios');
+
+// template connection to Infura Eth2.0 endpoint
+var infuraEndPoint = `https://${process.env.PROJECT_ID}:${process.env.PROJECT_SECRET}@eth2-beacon-mainnet.infura.io`;
+var infuraApiPath = '/eth/v1/beacon/genesis';
+var infuraUrl = infuraEndPoint + infuraApiPath;
+
+async function getRestApi(url) {
+	try{
+		const response = await axios.get(url);
+		console.log(response.data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+getRestApi(infuraUrl);
+
+// template connection to PostgreSQL on local server
+var postgresEndPoint = `https://${process.env.POSTGRES_DATABASE}:${process.env.POSTGRES_PASSWORD}@localhost:5432`;
+var tempSql = './temp.sql';
+
+async function getPostgreSql(endpoint, action) {
+	const postgresDB = createConnectionPool(endpoint);
+	const results = await postgresDB.query(sql.file(action));
+	console.log(results);
+	await postgresDB.dispose();
+}
+
+getPostgreSql(postgresEndPoint, tempSql).catch( (err) => {
+	console.error(err);
+	process.exit(1);
+})
+
+// template connection to Prometheus, a real-time streaming database
+const prometheusEndPoint = new PrometheusQuery.PrometheusDriver({
+	endpoint: "http://localhost:9091",
+	baseURL: "/api/v1"
+})
+
+const justified_epoch = 'beacon_current_justified_epoch{instance="localhost:8008",job="teku-client"}';
+// const justified_root = 'beacon_current_justified_root{instance="localhost:8008",job="teku-client"}';
+// const finalized_epoch = 'beacon_finalized_epoch{instance="localhost:8008",job="teku-client"}';
+// const finalized_root = 'beacon_finalized_root{instance="localhost:8008",job="teku-client"}';
+
+async function getPrometheus(task) {
+	try {
+		const responses = await prom.instantQuery(task);
+		for (res in responses.result) { console.log(responses.result[res].value.value); }
+	} catch (error) {
+		console.log(error);
+	};
+}
+
+getPrometheus(justified_epoch);
+
